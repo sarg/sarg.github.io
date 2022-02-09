f95ce03505ef89c3d9c8c4a8a6ec20eb6b329627
Sergey Trofimov <sarg@sarg.org.ru>
2022-02-09 14:57:17 +0100

    Extract grid filter to a js file

---
+++ js/filter.js
@@ -0,0 +1,21 @@
+var filter = {};
+
+function applyFilters() {
+  document.querySelectorAll("ul.ratings > li")
+    .forEach(el => el.hidden = Object.values(filter).find(f => !f(el)));
+}
+
+document.querySelectorAll("ul.tags > li").forEach(el => el.onclick = function(event) {
+  var active = event.srcElement.classList.toggle('active');
+
+     var tagName = event.srcElement.innerText;
+     var tag = event.srcElement.getAttribute('tag');
+     if (active) {
+         var rx = new RegExp('\\b' + tag + '\\b');
+         filter[tag] = el => rx.test(el.getAttribute('data-tags'));
+     } else {
+         delete filter[tag];
+     }
+
+  applyFilters();
+});
